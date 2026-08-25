eb318f4a25dd7c0cfcf03915f57abc49d8f36ff5
AnaLuiza1817 <ana_lb_carvalho@estudante.seisenai.org.br>
2026-08-25 08:51:56 -0300

    Add-Update no codigo do  bancop ded dados

--- Database/db.sql
+++ Database/db.sql
@@ -1,8 +1,11 @@
 CREATE DATABASE IF NOT EXISTS pet_shop;
 USE pet_shop;
 
+DROP TABLE IF EXISTS animais;
+DROP TABLE IF EXISTS clientes;
+
 CREATE TABLE clientes (
-    id_cliemte INT  AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     telefone VARCHAR(20) NOT NULL,
     email VARCHAR(100)
@@ -14,12 +17,7 @@ CREATE TABLE animais (
     especie VARCHAR(50) NOT NULL,
     raca VARCHAR(100),
     idade INT NOT NULL,
-    id_cliente INT NOT NULL
-)
-
-  CONSTRAINT fk_animal_cliente
-        FOREIGN KEY (id_cliente)
-        REFERENCES clientes(id_cliente)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    id_cliente INT NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
+
